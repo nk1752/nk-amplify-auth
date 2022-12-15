bdb354432f7130ffb29893ced05124311dd9efb1
nk1752 <71557302+nk1752@users.noreply.github.com>
2022-12-15 11:51:39 -0600

    add sign out button

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Auth } from 'aws-amplify';
+
+async function Signout() {
+
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.log('error signing out: ', error);
+    }
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Signout</h1>
       </header>
+      <button onClick={Signout}>Sign Out</button>
     </div>
   );
 }
